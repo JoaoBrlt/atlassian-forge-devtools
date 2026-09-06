0001031aaf4219f73608dabcf4bd51c7c83f73a3
João Brilhante <joao@brilhante.fr>
2026-09-06 21:07:38 +0200

    feat: improve request path readability (#109)

--- src/utils/http-utils.test.ts
+++ src/utils/http-utils.test.ts
@@ -1,4 +1,4 @@
-import { getSafeStatusText } from "./http-utils";
+import { getSafeStatusText, parseUrl } from "./http-utils";
 import { describe, expect, it } from "vitest";
 
 describe("getSafeStatusText", () => {
@@ -32,5 +32,128 @@ describe("getSafeStatusText", () => {
 
   it("should return 'Internal Server Error' for 500", () => {
     expect(getSafeStatusText(500)).toBe("Internal Server Error");
+  });
+});
+
+describe("parseUrl", () => {
+  it("should parse an absolute URL", () => {
+    expect(parseUrl("https://www.google.com/foo/bar")).toEqual({
+      protocol: "https",
+      host: "www.google.com",
+      pathname: "/foo/bar",
+      searchParams: [],
+    });
+  });
+
+  it("should parse an absolute URL with a port", () => {
+    expect(parseUrl("http://localhost:8080/foo/bar")).toEqual({
+      protocol: "http",
+      host: "localhost:8080",
+      pathname: "/foo/bar",
+      searchParams: [],
+    });
+  });
+
+  it("should parse an absolute URL without a path", () => {
+    expect(parseUrl("https://www.google.com")).toEqual({
+      protocol: "https",
+      host: "www.google.com",
+      pathname: "/",
+      searchParams: [],
+    });
+  });
+
+  it("should parse an absolute URL with query parameters", () => {
+    expect(parseUrl("https://www.google.com/foo/bar?foo=bar&foo2=bar2")).toEqual({
+      protocol: "https",
+      host: "www.google.com",
+      pathname: "/foo/bar",
+      searchParams: [
+        { name: "foo", value: "bar" },
+        { name: "foo2", value: "bar2" },
+      ],
+    });
+  });
+
+  it("should parse a relative URL", () => {
+    expect(parseUrl("/rest/api/3/issue")).toEqual({
+      pathname: "/rest/api/3/issue",
+      searchParams: [],
+    });
+  });
+
+  it("should parse a relative URL with query parameters", () => {
+    expect(parseUrl("/rest/api/3/search?jql=project%20%3D%20PROJ&maxResults=50")).toEqual({
+      pathname: "/rest/api/3/search",
+      searchParams: [
+        { name: "jql", value: "project = PROJ" },
+        { name: "maxResults", value: "50" },
+      ],
+    });
+  });
+
+  it("should parse repeated query parameters", () => {
+    expect(parseUrl("/rest/api/3/issue?fields=summary&fields=status")).toEqual({
+      pathname: "/rest/api/3/issue",
+      searchParams: [
+        { name: "fields", value: "summary" },
+        { name: "fields", value: "status" },
+      ],
+    });
+  });
+
+  it("should parse a query parameter without a value", () => {
+    expect(parseUrl("/rest/api/3/issue?expand")).toEqual({
+      pathname: "/rest/api/3/issue",
+      searchParams: [{ name: "expand", value: "" }],
+    });
+  });
+
+  it("should parse an absolute URL with a hash", () => {
+    expect(parseUrl("https://www.google.com/foo/bar#section")).toEqual({
+      protocol: "https",
+      host: "www.google.com",
+      pathname: "/foo/bar",
+      searchParams: [],
+      hash: "section",
+    });
+  });
+
+  it("should parse a relative URL with a hash", () => {
+    expect(parseUrl("/rest/api/3/issue#comments")).toEqual({
+      pathname: "/rest/api/3/issue",
+      searchParams: [],
+      hash: "comments",
+    });
+  });
+
+  it("should parse a URL with query parameters and a hash", () => {
+    expect(parseUrl("/rest/api/3/issue?fields=summary#comments")).toEqual({
+      pathname: "/rest/api/3/issue",
+      searchParams: [{ name: "fields", value: "summary" }],
+      hash: "comments",
+    });
+  });
+
+  it("should parse a hash containing separators", () => {
+    expect(parseUrl("/foo#/bar?baz=1")).toEqual({
+      pathname: "/foo",
+      searchParams: [],
+      hash: "/bar?baz=1",
+    });
+  });
+
+  it("should ignore an empty hash", () => {
+    expect(parseUrl("/rest/api/3/issue#")).toEqual({
+      pathname: "/rest/api/3/issue",
+      searchParams: [],
+      hash: undefined,
+    });
+  });
+
+  it("should return the raw URL when it cannot be parsed", () => {
+    expect(parseUrl("http://")).toEqual({
+      pathname: "http://",
+    });
   });
 });
--- src/utils/http-utils.ts
+++ src/utils/http-utils.ts
@@ -1,5 +1,7 @@
 import HttpStatus from "http-status-codes";
 
+const RELATIVE_URL_BASE = "http://localhost";
+
 /**
  * Returns the reason phrase for the given status code.
  * @param status the status code
@@ -11,4 +13,69 @@ export function getSafeStatusText(status: number) {
   } catch {
     return "Unknown";
   }
+}
+
+export interface UrlSearchParam {
+  name: string;
+  value: string;
+}
+
+export interface ParsedUrl {
+  protocol?: string;
+  host?: string;
+  pathname: string;
+  searchParams?: UrlSearchParam[];
+  hash?: string;
+}
+
+export function parseUrl(url: string): ParsedUrl {
+  const absoluteUrl = URL.parse(url);
+  if (absoluteUrl != null) {
+    return {
+      protocol: parseProtocol(absoluteUrl.protocol),
+      host: parseHost(absoluteUrl.host),
+      pathname: absoluteUrl.pathname,
+      searchParams: parseSearchParams(absoluteUrl.searchParams),
+      hash: parseHash(absoluteUrl.hash),
+    };
+  }
+
+  const relativeUrl = URL.parse(url, RELATIVE_URL_BASE);
+  if (relativeUrl != null) {
+    return {
+      pathname: relativeUrl.pathname,
+      searchParams: parseSearchParams(relativeUrl.searchParams),
+      hash: parseHash(relativeUrl.hash),
+    };
+  }
+
+  return { pathname: url };
+}
+
+function parseProtocol(protocol: string | undefined | null): string | undefined {
+  if (protocol == null || protocol.length === 0) {
+    return undefined;
+  }
+  return protocol.replace(":", "");
+}
+
+function parseHost(host: string | undefined | null): string | undefined {
+  if (host == null || host.length === 0) {
+    return undefined;
+  }
+  return host;
+}
+
+function parseSearchParams(searchParams: URLSearchParams | undefined | null): UrlSearchParam[] | undefined {
+  if (searchParams == null) {
+    return undefined;
+  }
+  return Array.from(searchParams, ([name, value]) => ({ name, value }));
+}
+
+function parseHash(hash: string | undefined | null): string | undefined {
+  if (hash == null || hash.length === 0) {
+    return undefined;
+  }
+  return hash.slice(1);
 }
